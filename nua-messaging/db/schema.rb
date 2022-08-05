4dcdc15b7a561cc29789cc9c167796d763270e31
zanetagebka <zanetagebka@gmail.com>
2022-08-05 11:46:19 +0200

    Task 2
    - schema update

--- nua-messaging/db/schema.rb
+++ nua-messaging/db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_153436) do
+ActiveRecord::Schema.define(version: 2022_08_05_084715) do
 
   create_table "inboxes", force: :cascade do |t|
     t.integer "user_id"
+    t.integer "unread"
     t.index ["user_id"], name: "index_inboxes_on_user_id"
   end
 
